7e457deb093f827c47eba90d0f04490fc3a2615c
Matt Pedersen <mpedersen2054@gmail.com>
2014-12-01 20:31:53 -0600

    add showResults function that grabs all the needed info into vars, and them inserts the needed info into a new user for each one present

--- js/app.js
+++ js/app.js
@@ -21,4 +21,38 @@ $(function() {
         });
     }
 
+    function showResults(results) {
+
+        for (var i=0; i<results.length; i++) {
+            var img = results[i].user.profile_image,
+                name = results[i].user.display_name,
+                link = results[i].user.link,
+                score = results[i].score,
+                numPosts = results[i].post_count;
+
+            var template = intoTemplate(img,name,link);
+            $('#results .container').append(template);
+        }
+
+        function intoTemplate(img,name,link) {
+            var templ = '';
+            templ+="<li class='user'>"
+            templ+="<div class='left-meta'>"
+            templ+="<img src='"+img+"'>"
+            templ+="</div>"
+            templ+="<div class='right-meta'>"
+            templ+="<div class='name'>"
+            templ+="<h3><a href='"+link+"'>"+name+"</a></h3>"
+            templ+="</div>"
+            templ+="<div class='stats'>"
+            templ+="<p>post count: <span class='post-count'>"+numPosts+"</span></p>"
+            templ+="<p>score: <span class='score'>"+score+"</span></p>"
+            templ+="</div>"
+            templ+="</div>"
+            templ+="</li>"
+            return templ;
+        }
+
+    }
+
 })();
